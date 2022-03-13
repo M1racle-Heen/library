496e409bb735659c567c0b14530ba76f89e0548c
M1racle-Heen <190109003@stu.sdu.edu.kz>
2022-03-13 13:52:52 +0600

    done with testings bookInteractor methods

--- internal/usecase/interactor/book_interactor.go
+++ internal/usecase/interactor/book_interactor.go
@@ -21,9 +21,7 @@ func NewBookInteractor(bookRepo repository.BookRepo) usecase.BookUsecase {
 
 }
 
-func (interactor bookInteractor) Create(
-	book domain.Book,
-) (*domain.Book, error) {
+func (interactor bookInteractor) Create(book domain.Book) (*domain.Book, error) {
 
 	createdBook, err := interactor.bookRepo.Store(book)
 
@@ -35,9 +33,7 @@ func (interactor bookInteractor) Create(
 
 }
 
-func (interactor bookInteractor) Get(
-	id string,
-) (*domain.Book, error) {
+func (interactor bookInteractor) Get(id string) (*domain.Book, error) {
 	book, err := interactor.bookRepo.Get(id)
 
 	if err != nil {
@@ -51,3 +47,56 @@ func (interactor bookInteractor) Get(
 func (interactor bookInteractor) Delete(id string) error {
 	return interactor.bookRepo.Remove(id)
 }
+
+// func TestBookInteractor_Get(t *testing.T) {
+// 	type arguments struct {
+// 		id string
+// 	}
+// 	type expectations struct {
+// 		getBook *domain.Book
+// 		err     error
+// 	}
+// 	type dependencies struct {
+// 		bookRepo *mocks.BookRepo
+// 	}
+
+// 	type test struct {
+// 		arguments    arguments
+// 		expectations expectations
+// 		dependencies dependencies
+// 		prepare      func(bookRepo *mocks.BookRepo, getBook *domain.Book, err error)
+// 	}
+
+// 	id := "123"
+// 	book := domain.Book{}
+// 	testCases := []test{
+// 		{
+// 			arguments:    arguments{id: id},
+// 			expectations: expectations{getBook: &book, err: nil},
+// 			dependencies: dependencies{bookRepo: &mocks.BookRepo{}},
+// 			prepare: func(bookRepo *mocks.BookRepo, getBook *domain.Book, err error) {
+// 				bookRepo.On("Get", mock.Anything).Return(getBook, err)
+// 			},
+// 		},
+// 		{
+// 			arguments:    arguments{id: id},
+// 			expectations: expectations{getBook: nil, err: errors.New("Some error")},
+// 			dependencies: dependencies{bookRepo: &mocks.BookRepo{}},
+// 			prepare: func(bookRepo *mocks.BookRepo, getBook *domain.Book, err error) {
+// 				bookRepo.On("Get", mock.Anything).Return(getBook, err)
+// 			},
+// 		},
+// 	}
+
+// 	for _, tc := range testCases {
+
+// 		tc.prepare(tc.dependencies.bookRepo, tc.expectations.getBook, tc.expectations.err)
+
+// 		interactor := bookInteractor{bookRepo: tc.dependencies.bookRepo}
+
+// 		getBook, err := interactor.Get(tc.arguments.id)
+
+// 		assert.Equal(t, tc.expectations.getBook, getBook)
+// 		assert.Equal(t, tc.expectations.err, err)
+// 	}
+// }
--- internal/usecase/interactor/book_interactor_test.go
+++ internal/usecase/interactor/book_interactor_test.go
@@ -128,3 +128,103 @@ func TestBookInteractor_Create(t *testing.T) {
 	}
 
 }
+
+func TestBookInteractor_Get(t *testing.T) {
+	type arguments struct {
+		id string
+	}
+	type expectations struct {
+		getBook *domain.Book
+		err     error
+	}
+	type dependencies struct {
+		bookRepo *mocks.BookRepo
+	}
+
+	type test struct {
+		arguments    arguments
+		expectations expectations
+		dependencies dependencies
+		prepare      func(bookRepo *mocks.BookRepo, getBook *domain.Book, err error)
+	}
+
+	id := "123"
+	book := domain.Book{}
+	testCases := []test{
+		{
+			arguments:    arguments{id: id},
+			expectations: expectations{getBook: &book, err: nil},
+			dependencies: dependencies{bookRepo: &mocks.BookRepo{}},
+			prepare: func(bookRepo *mocks.BookRepo, getBook *domain.Book, err error) {
+				bookRepo.On("Get", mock.Anything).Return(getBook, err)
+			},
+		},
+		{
+			arguments:    arguments{id: id},
+			expectations: expectations{getBook: nil, err: errors.New("Some error")},
+			dependencies: dependencies{bookRepo: &mocks.BookRepo{}},
+			prepare: func(bookRepo *mocks.BookRepo, getBook *domain.Book, err error) {
+				bookRepo.On("Get", mock.Anything).Return(getBook, err)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+
+		tc.prepare(tc.dependencies.bookRepo, tc.expectations.getBook, tc.expectations.err)
+
+		interactor := bookInteractor{bookRepo: tc.dependencies.bookRepo}
+
+		getBook, err := interactor.Get(tc.arguments.id)
+
+		assert.Equal(t, tc.expectations.getBook, getBook)
+		assert.Equal(t, tc.expectations.err, err)
+	}
+}
+
+func TestBookInteractor_Delete(t *testing.T) {
+	type arguments struct {
+		id string
+	}
+	type expectations struct {
+		err error
+	}
+	type dependencies struct {
+		bookRepo *mocks.BookRepo
+	}
+
+	type test struct {
+		arguments    arguments
+		expectations expectations
+		dependencies dependencies
+		prepare      func(bookRepo *mocks.BookRepo, err error)
+	}
+
+	id := "123"
+
+	testCases := []test{
+		{
+			arguments:    arguments{id: id},
+			expectations: expectations{err: nil},
+			dependencies: dependencies{bookRepo: &mocks.BookRepo{}},
+			prepare: func(bookRepo *mocks.BookRepo, err error) {
+				bookRepo.On("Remove", mock.Anything).Return(err)
+			},
+		},
+		{
+			arguments:    arguments{id: id},
+			expectations: expectations{err: errors.New("Some error")},
+			dependencies: dependencies{bookRepo: &mocks.BookRepo{}},
+			prepare: func(bookRepo *mocks.BookRepo, err error) {
+				bookRepo.On("Remove", mock.Anything).Return(err)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc.prepare(tc.dependencies.bookRepo, tc.expectations.err)
+		interactor := bookInteractor{bookRepo: tc.dependencies.bookRepo}
+		err := interactor.Delete(tc.arguments.id)
+		assert.Equal(t, tc.expectations.err, err)
+	}
+}
